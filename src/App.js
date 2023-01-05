e5898c6ce80cf98ed9c054ba733eb5175283a222
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-05 13:58:21 +0600

    added add by payload

--- src/App.js
+++ src/App.js
@@ -8,15 +8,21 @@ function App() {
   const increment = () => {
     dispatch({ type: "INC" })
   };
+
   const decrement = () => {
     dispatch({ type: "DEC" })
   };
+
+  const AddBy = () => {
+    dispatch({ type: "ADD", payload: 10 });
+  }
 
   return (
     <div>
       <h1>{counter}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={AddBy}>Add By</button>
     </div>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -9,6 +9,10 @@ const reducerFn = (state = { counter: 10 }, action) => {
     return { counter: state.counter - 1 }
   }
 
+  if (action.type === "ADD") {
+    return { counter: state.counter + action.payload }
+  }
+
   return state;
 };
 
