4357599aa003a6f116d5b1f4a307c1a59207b325
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-01-05 14:07:36 +0600

    implement redux using redux toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,18 @@
-import { createStore } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-const reducerFn = (state = { counter: 10 }, action) => {
-  if (action.type === "INC") {
-    return { counter: state.counter + 1 }
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    counter: 0
+  },
+  reducers: {
+    increment(action, state) {
+
+    },
+    decrement(action, state) {
+
+    },
+    addBy(action, state) { }
   }
 
-  if (action.type === "DEC") {
-    return { counter: state.counter - 1 }
-  }
-
-  if (action.type === "ADD") {
-    return { counter: state.counter + action.payload }
-  }
-
-  return state;
-};
-
-const store = createStore(reducerFn);
-export default store;
+});
